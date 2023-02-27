4c4a6279cb981cc0ff73f250577f33f06750ac98
Dmitry Radkovskiy <zlumer@gmail.com>
2023-02-27 16:01:31 +0000

    [477850] another batch of optimizations

--- exercises/gas/contracts/Gas.sol
+++ exercises/gas/contracts/Gas.sol
@@ -2,30 +2,29 @@
 pragma solidity 0.8.19;
 
 struct Payment {
-    uint8 paymentType;
-    uint16 amount;
+    uint256 paymentType;
+    uint256 amount;
 }
 
 contract GasContract {
-    uint16 public constant totalSupply = 10000; // cannot be updated
-    mapping(address => uint16) private balances;
+    mapping(address => uint256) public whitelist;
+    mapping(address => uint256) private balances;
     mapping(address => Payment[]) private payments;
-    mapping(address => uint8) public whitelist;
     address[5] public administrators;
 
-    event Transfer(address recipient, uint16 amount);
+    event Transfer(address, uint256);
 
     constructor(address[5] memory _admins, uint256) {
         administrators = _admins;
-        assembly { // balances[msg.sender] = totalSupply;
-            mstore(0x0, caller())
-            mstore(0x20, balances.slot)
-            let slot := keccak256(0x0, 0x40)
-            sstore(slot, totalSupply)
+    }
+
+    function totalSupply() external pure returns (uint256) {
+        unchecked {
+            return 10000;
         }
     }
 
-    function balanceOf(address _user) external view returns (uint16) {
+    function balanceOf(address _user) external view returns (uint256) {
         return balances[_user];
     }
 
@@ -39,7 +38,7 @@ contract GasContract {
 
     function transfer(
         address _recipient,
-        uint16 _amount,
+        uint256 _amount,
         string calldata
     ) external returns (bool) {
         // balances[msg.sender] = balances[msg.sender] - _amount;
@@ -59,12 +58,12 @@ contract GasContract {
 
     function updatePayment(
         address _user,
-        uint8 idx,
-        uint16 _amount,
-        uint8 _type
+        uint256 idx,
+        uint256 _amount,
+        uint256 _type
     ) external {
         // bool allowed = false;
-        // for (uint8 i = 0; i < administrators.length;) {
+        // for (uint256 i = 0; i < administrators.length;) {
         //     if (administrators[i] == msg.sender) {
         //         allowed = true;
         //         break;
@@ -82,11 +81,11 @@ contract GasContract {
         }
     }
 
-    function addToWhitelist(address _userAddrs, uint8 _tier) external {
+    function addToWhitelist(address _userAddrs, uint256 _tier) external {
         // whitelist[_userAddrs] = _tier;
         assembly {
             mstore(0x0, _userAddrs)
-            mstore(0x20, whitelist.slot)
+            // mstore(0x20, whitelist.slot)
             let slot := keccak256(0x0, 0x40)
             sstore(slot, _tier)
         }
@@ -94,25 +93,25 @@ contract GasContract {
 
     function whiteTransfer(
         address _recipient,
-        uint16 _amount,
-        uint64[3] calldata
+        uint256 _amount,
+        uint256[3] calldata
     ) external {
         assembly {
-            // uint16 senderAmount = whitelist[msg.sender];
+            // uint256 senderAmount = whitelist[msg.sender];
             mstore(0x0, caller())
-            mstore(0x20, whitelist.slot)
+            // mstore(0x20, whitelist.slot)
             let slot := keccak256(0x0, 0x40)
             let senderAmount := sload(slot)
 
             let total := sub(_amount, senderAmount)
 
-            // uint16 senderBalance = balances[msg.sender];
+            // uint256 senderBalance = balances[msg.sender];
             // mstore(0x0, caller()) -- it's already in memory
             mstore(0x20, balances.slot)
             slot := keccak256(0x0, 0x40)
             sstore(slot, sub(sload(slot), total))
             
-            // uint16 recipientBalance = balances[_recipient];
+            // uint256 recipientBalance = balances[_recipient];
             mstore(0x0, _recipient)
             // mstore(0x20, balances.slot) -- it's already in memory
             slot := keccak256(0x0, 0x40)
